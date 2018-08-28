cb602bfdc247218329a3e3f4eb470a217c7aa4b9
eduardosgit <eduardosrodrigues@outlook.com>
2018-08-27 23:10:37 -0300

    adicao do campo nome

--- src/main/java/br/edu/iftm/extensao/dominio/Teste.java
+++ src/main/java/br/edu/iftm/extensao/dominio/Teste.java
@@ -1,5 +1,14 @@
 package br.edu.iftm.extensao.dominio;
 
 public class Teste {
+	
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
